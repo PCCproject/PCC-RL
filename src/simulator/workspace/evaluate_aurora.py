3f67211491df319590ffe50c7824cdff89b3f66d
Zhengxu Xia <kevinxzx95@gmail.com>
2021-03-08 23:22:51 -0600

    Refactor evaluation code.

--- src/simulator/workspace/evaluate_aurora.py
+++ src/simulator/workspace/evaluate_aurora.py
@@ -15,7 +15,7 @@ from common.utils import set_tf_loglevel
 # from simulator.network_simulator import network
 # from simulator import network
 from simulator.aurora import Aurora
-from simulator.trace import generate_traces
+from simulator.trace import generate_traces, generate_trace
 
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 warnings.filterwarnings("ignore")
@@ -34,8 +34,17 @@ def parse_args():
     parser.add_argument('--seed', type=int, default=42, help='seed')
     parser.add_argument('--duration', type=int, default=None,
                         help='Flow duration in seconds.')
-    parser.add_argument("--config-file", type=str,
-                        required=True, help='config file.')
+    parser.add_argument("--config-file", type=str, default=None,
+                        help='config file.')
+
+    parser.add_argument('--delay', type=float, default=50,
+                        help="one-way delay. Unit: millisecond.")
+    parser.add_argument('--bandwidth', type=float, default=2,
+                        help="Constant bandwidth. Unit: mbps.")
+    parser.add_argument('--loss', type=float, default=0,
+                        help="Constant random loss of uplink.")
+    parser.add_argument('--queue', type=int, default=100,
+                        help="Uplink queue size. Unit: packets.")
 
     return parser.parse_args()
 
@@ -45,25 +54,36 @@ def main():
     if args.save_dir:
         os.makedirs(args.save_dir, exist_ok=True)
 
-    test_traces = generate_traces(
-        args.config_file, 10, args.duration, args.seed)
-    training_traces = generate_traces(
-        args.config_file, 10, args.duration, args.seed)
-    # env = gym.make('PccNs-v0', log_dir=args.save_dir, duration=args.duration)
-    # env.seed(args.seed)
+    if args.config_file is not None:
+        test_traces = generate_traces(args.config_file, 10, args.duration)
+    else:
+        test_traces = [generate_trace(args.duration,
+                                      (args.bandwidth, args.bandwidth),
+                                      (args.delay, args.delay),
+                                      (args.loss, args.loss),
+                                      (args.queue, args.queue))]
 
-    aurora = Aurora(training_traces, timesteps_per_actorbatch=10,
-                    log_dir=args.save_dir, seed=args.seed,
+    aurora = Aurora(args.seed, timesteps_per_actorbatch=10,
+                    log_dir=args.save_dir,
                     pretrained_model_path=args.model_path)
     results = aurora.test(test_traces)
 
-    for idx, (trace, result) in enumerate(zip(test_traces, results)):
-        with open(os.path.join(args.save_dir, "env_{:.3f}_{:.3f}_{:.3f}_{:.3f}.csv".format(
-                trace.bandwidths[0], trace.delay, trace.loss_rate, trace.queue_size)), 'w', 1) as f:
-            log_writer = csv.writer(f, delimiter='\t', lineterminator='\n')
-            log_writer.writerow(['timestamp', 'mean_validation_reward', 'loss',
-                                 'throughput', 'latency', 'sending_rate', 'action'])
-            log_writer.writerows(result)
+    for _, (trace, result) in enumerate(zip(test_traces, results)):
+        log_path = os.path.join(args.save_dir,
+                                "env_{:.3f}_{:.3f}_{:.3f}_{:.3f}.csv".format(
+                                    trace.bandwidths[0], trace.delay,
+                                    trace.loss_rate, trace.queue_size))
+        with open(log_path, 'w', 1) as f:
+            f.write("\t".join(['timestamp', 'reward',
+                               'sending_rate', 'throughput', 'latency', 'loss',
+                               'action']) + "\n")
+            for line in result:
+                f.write("{timestamp:.3f}\t{reward:.3f}\t{sending_rate:.3f}\t"
+                        "{throughput:.3f}\t{latency:.3f}\t{loss:.3f}\t"
+                        "{action:.3f}\n".format(
+                            timestamp=line[0], reward=line[1],
+                            sending_rate=line[2], throughput=line[3],
+                            latency=line[4], loss=line[5], action=line[6]))
 
 
 if __name__ == "__main__":
