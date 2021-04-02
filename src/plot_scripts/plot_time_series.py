ce00d51552c641ebec3ca5ddd8df13b2cbb31263
Zhengxu Xia <kevinxzx95@gmail.com>
2021-04-02 13:55:28 -0500

    Better visualization.

--- src/plot_scripts/plot_time_series.py
+++ src/plot_scripts/plot_time_series.py
@@ -25,7 +25,7 @@ def main():
     print(args.log_file)
     for log_file in args.log_file:
         df = pd.read_csv(log_file)
-        fig, axes = plt.subplots(6, 1, figsize=(18, 16))
+        fig, axes = plt.subplots(5, 1, figsize=(10, 10))
 
         axes[0].plot(df['timestamp'], df['throughput'] * 1500 * 8 / 1000000,
                      label='throughput, avg {:.3f}mbps'.format(df['throughput'].mean() * 1500 * 8 / 1000000))
@@ -38,7 +38,7 @@ def main():
         axes[0].set_xlabel("Time(s)")
         axes[0].set_ylabel("mbps")
         axes[0].legend()
-        axes[0].set_ylim(0, )
+        axes[0].set_ylim(0, 6)
         axes[0].set_xlim(0, )
 
         axes[1].plot(df['timestamp'], df['latency']*1000,
@@ -49,35 +49,37 @@ def main():
         axes[1].set_ylabel("Latency(ms)")
         axes[1].legend()
         axes[1].set_xlim(0, )
+        axes[1].set_ylim(0, 80)
 
-        axes[2].plot(df['timestamp'], df['reward'],
-                     label='rewards avg {:.3f}'.format(df['reward'].mean()))
+
+        axes[2].plot(df['timestamp'], df['loss'], label='loss avg {:.3f}'.format(df['loss'].mean()))
         axes[2].set_xlabel("Time(s)")
-        axes[2].set_ylabel("Reward")
+        axes[2].set_ylabel("loss")
         axes[2].legend()
         axes[2].set_xlim(0, )
+        axes[2].set_ylim(0, 1)
 
-        if "action" in df:
-            axes[3].plot(df['timestamp'], df['action'] * 1.0, label='delta')
-            axes[3].set_xlabel("Time(s)")
-            axes[3].set_ylabel("delta")
-            axes[3].legend()
-            axes[3].set_xlim(0, )
+        axes[3].plot(df['timestamp'], df['reward'],
+                     label='rewards avg {:.3f}'.format(df['reward'].mean()))
+        axes[3].set_xlabel("Time(s)")
+        axes[3].set_ylabel("Reward")
+        axes[3].legend()
+        axes[3].set_xlim(0, )
 
-        axes[4].plot(df['timestamp'], df['loss'],
-                     label='loss avg {:.3f}'.format(df['loss'].mean()))
+        axes[4].plot(df['timestamp'], df['action'] * 1.0, label='delta')
         axes[4].set_xlabel("Time(s)")
-        axes[4].set_ylabel("loss")
+        axes[4].set_ylabel("delta")
         axes[4].legend()
         axes[4].set_xlim(0, )
 
-        axes[5].plot(df['timestamp'], df['cwnd'], label='cwnd')
-        axes[5].plot(df['timestamp'], df['ssthresh'], label='ssthresh')
-        axes[5].set_xlabel("Time(s)")
-        axes[5].set_ylabel("# packets")
-        axes[5].set_ylim(0, df['cwnd'].max())
-        axes[5].legend()
-        axes[5].set_xlim(0, )
+
+        # axes[5].plot(df['timestamp'], df['cwnd'], label='cwnd')
+        # axes[5].plot(df['timestamp'], df['ssthresh'], label='ssthresh')
+        # axes[5].set_xlabel("Time(s)")
+        # axes[5].set_ylabel("# packets")
+        # axes[5].set_ylim(0, df['cwnd'].max())
+        # axes[5].legend()
+        # axes[5].set_xlim(0, )
         print(df['reward'].mean())
         plt.tight_layout()
         if args.save_dir is not None:
