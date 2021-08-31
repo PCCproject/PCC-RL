e248861ede15926527a273f65fb2e8a795201837
Zhengxu Xia <kevinxzx95@gmail.com>
2021-08-30 22:58:20 -0500

    Remove comments and unused imports.

--- src/simulator/train_rl.py
+++ src/simulator/train_rl.py
@@ -1,15 +1,10 @@
 import argparse
-import itertools
 import os
 import time
 import warnings
 
-import ipdb
 from mpi4py.MPI import COMM_WORLD
-import numpy as np
 
-from simulator import network
-from simulator.trace import generate_trace, generate_traces
 from simulator.aurora import Aurora
 from common.utils import set_seed
 
@@ -81,31 +76,6 @@ def main():
     log_dir = args.save_dir
     os.makedirs(log_dir, exist_ok=True)
     set_seed(args.seed + COMM_WORLD.Get_rank() * 100)
-
-    # if args.randomization_range_file is not None:
-    #     # generate training traces
-    #     training_traces = generate_traces(args.randomization_range_file,
-    #                                       args.total_trace_count,
-    #                                       args.duration,
-    #                                       constant_bw=not args.time_variant_bw)
-    #     # generate validation traces
-    #     validation_traces = generate_traces(
-    #         args.randomization_range_file, 50, args.duration,
-    #         constant_bw=not args.time_variant_bw)
-    # else:
-    #     # generate training traces
-    #     training_traces = [generate_trace(args.duration, args.bandwidth,
-    #                                       args.delay, args.loss, args.queue,
-    #                                       constant_bw=not args.time_variant_bw)
-    #                        for _ in range(args.total_trace_count)]
-    #     # generate validation traces
-    #     validation_traces = [generate_trace(args.duration, (bw, bw),
-    #                                         (lat, lat), (loss, loss),
-    #                                         (queue, queue),
-    #                                         constant_bw=not args.time_variant_bw)
-    #                          for bw, lat, loss, queue in itertools.product(
-    #         args.val_bandwidth, args.val_delay,
-    #         args.val_loss, args.val_queue)]
 
     # Initialize model and agent policy
     aurora = Aurora(args.seed + COMM_WORLD.Get_rank() * 100, args.save_dir, 1800, #7200,
