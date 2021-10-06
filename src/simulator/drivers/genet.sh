d6b03df090ebb848300c3d1ac113bd400fb6ce2d
Zhengxu Xia <kevinxzx95@gmail.com>
2021-10-06 16:42:33 -0500

    Commit genet.sh from farewell

--- src/simulator/drivers/genet.sh
+++ src/simulator/drivers/genet.sh
@@ -1,20 +1,23 @@
 set -e
-SAVE_DIR=../../results_0826/genet_cubic_exp_2
-CUDA_VISIBLE_DEVICES="" mpiexec -np 2 python genet.py \
+SAVE_DIR=../../results_0911/genet_cubic_noise_udr_start
+CUDA_VISIBLE_DEVICES="" mpiexec -np 8 python genet.py \
     --heuristic cubic \
     --save-dir ${SAVE_DIR} \
-    --config-file ../../config/train/udr_7_dims_0826/udr_large.json \
+    --config-file ../../config/train/udr_7_dims_0911/udr_large.json \
     --bo-rounds 200 \
-    --model-path ../../results_0826/udr_7/udr_large/seed_10/model_step_21600.ckpt
+    --model-path /tank/zxxia/PCC-RL/results_0826/udr_6/udr_start/seed_20/model_step_21600.ckpt &
+    # --model-path ../../results_0826/udr_7/udr_large/seed_10/model_step_21600.ckpt &
     # --model-path ../../results_0826/udr_6/udr_large/seed_20/model_step_21600.ckpt
     # --model-path ../../results_0826/udr_3/udr_large/seed_20/model_step_151200.ckpt
 
-SAVE_DIR=../../results_0826/genet_bbr_exp_2
-CUDA_VISIBLE_DEVICES="" mpiexec -np 2 python genet.py \
+# SAVE_DIR=../../results_0826/genet_bbr_delete
+SAVE_DIR=../../results_0911/genet_bbr_noise_udr_start
+CUDA_VISIBLE_DEVICES="" mpiexec -np 8 python genet.py \
     --heuristic bbr \
     --save-dir ${SAVE_DIR}/ \
-    --config-file ../../config/train/udr_7_dims_0826/udr_large.json \
+    --config-file ../../config/train/udr_7_dims_0911/udr_large.json \
     --bo-rounds 200 \
-    --model-path ../../results_0826/udr_7/udr_large/seed_10/model_step_36000.ckpt
+    --model-path /tank/zxxia/PCC-RL/results_0826/udr_6/udr_start/seed_20/model_step_21600.ckpt &
+# ../../results_0826/udr_7/udr_large/seed_10/model_step_21600.ckpt &
     # --model-path ../../results_0826/udr_6/udr_large/seed_20/model_step_21600.ckpt
     # --model-path ../../results_0826/udr_3/udr_large/seed_20/model_step_21600.ckpt
