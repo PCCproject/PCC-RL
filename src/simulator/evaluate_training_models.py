dc198d8ff1276920a4847ae4b0f3d4fc0f127287
Zhengxu Xia <kevinxzx95@gmail.com>
2022-01-01 10:41:51 -0600

    Use abstraction of pantheon dataset.

--- src/simulator/evaluate_training_models.py
+++ src/simulator/evaluate_training_models.py
@@ -2,10 +2,8 @@
 Used to generate Figure 12.
 """
 import argparse
-import glob
 import os
 
-from tqdm import tqdm
 import pandas as pd
 
 import numpy as np
@@ -15,7 +13,7 @@ from simulator.network_simulator.bbr import BBR
 from simulator.network_simulator.bbr_old import BBR_old
 from simulator.network_simulator.cubic import Cubic
 # from simulator.network_simulator.pcc.vivace.vivace_latency import VivaceLatency
-from simulator.trace import Trace
+from simulator.pantheon_dataset import PantheonDataset
 
 # UDR1_ROOT = '/Users/zxxia/Project/PCC-RL/models/udr_small_lossless/seed_20'
 # UDR2_ROOT = '/Users/zxxia/Project/PCC-RL/models/udr_mid_lossless/seed_20'
@@ -47,7 +45,7 @@ def parse_args():
                         help="direcotry to testing results.")
     parser.add_argument('--cc', type=str, required=True,
                         choices=("bbr", 'bbr_old', "cubic", "udr1", "udr2", "udr3",
-                                 "genet_bbr", 'genet_bbr_old', 'genet_cubic'),
+                                 "genet_bbr", 'genet_bbr_old', 'genet_cubic', 'real'),
                         help='congestion control name')
     parser.add_argument("--conn-type", type=str, required=True,
                         choices=('ethernet', 'cellular', 'wifi'),
@@ -69,85 +67,83 @@ def main():
         queue_size = 50
     else:
         raise ValueError
-    link_dirs = glob.glob(os.path.join(TRACE_ROOT, args.conn_type, "*/"))
-    for link_dir in link_dirs:
-        traces = []
-        save_dirs = []
-        link_name = link_dir.split('/')[-2]
-        for cc in TARGET_CCS:
-            print("Loading real traces collected by {}...".format(cc))
-            for trace_file in tqdm(sorted(glob.glob(os.path.join(link_dir, "{}_datalink_run[1-3].log".format(cc))))):
-                traces.append(Trace.load_from_pantheon_file(
-                    trace_file, 0.0, queue_size, front_offset=0))
-                save_dir = os.path.join(args.save_dir, args.conn_type, link_name, os.path.splitext(
-                    os.path.basename(trace_file))[0])
-                os.makedirs(save_dir, exist_ok=True)
-                save_dirs.append(save_dir)
-        if args.cc == 'bbr':
-            cc = BBR(False)
-            cc.test_on_traces(traces, [os.path.join(save_dir, cc.cc_name)
-                                       for save_dir in save_dirs], False, args.nproc)
-        elif args.cc == 'bbr_old':
-            cc = BBR_old(False)
-            cc.test_on_traces(traces, [os.path.join(save_dir, cc.cc_name)
-                                       for save_dir in save_dirs], False, args.nproc)
-        elif args.cc == 'cubic':
-            cc = Cubic(False)
-            cc.test_on_traces(traces, [os.path.join(save_dir, cc.cc_name)
-                                       for save_dir in save_dirs], False, args.nproc)
-        elif args.cc == 'udr1' or args.cc == 'udr2' or args.cc == 'udr3':
-            # TODO: bug here when there is no validation log
-            # original implementation
-            # val_log = pd.read_csv(os.path.join(
-            #     args.models_path, 'validation_log.csv'), sep='\t')
-            # for idx in np.linspace(0, len(val_log['num_timesteps']) / 2 - 1, 10):
-            #     step = int(val_log['num_timesteps'].iloc[int(idx)])
-            #     udr_seed = ''
-            #     for s in args.models_path.split('/'):
-            #         if 'seed' in s:
-            #             udr_seed = s
-            #     udr_save_dirs = [os.path.join(
-            #         save_dir, args.cc, udr_seed, "step_{}".format(step)) for save_dir in save_dirs]
-            #     model_path = os.path.join(
-            #         args.models_path, 'model_step_{}.ckpt'.format(step))
-            #     test_on_traces(model_path, traces, udr_save_dirs,
-            #                    args.nproc, 42, False, False)
-            # new implementation to be fair with genet
-            udr_seed = ''
-            for s in args.models_path.split('/'):
-                if 'seed' in s:
-                    udr_seed = s
+    dataset = PantheonDataset(TRACE_ROOT, args.conn_type, post_nsdi=False,
+                              target_ccs=TARGET_CCS)
+    traces = dataset.get_traces(0, queue_size, front_offset=5, wrap=True)
+    save_dirs = [os.path.join(args.save_dir, args.conn_type, link_name,
+                              trace_name) for link_name, trace_name in dataset.trace_names]
+
+    if args.cc == 'bbr':
+        cc = BBR(False)
+        cc.test_on_traces(traces, [os.path.join(save_dir, cc.cc_name)
+                                   for save_dir in save_dirs], True, args.nproc)
+    elif args.cc == 'bbr_old':
+        cc = BBR_old(False)
+        cc.test_on_traces(traces, [os.path.join(save_dir, cc.cc_name)
+                                   for save_dir in save_dirs], True, args.nproc)
+    elif args.cc == 'cubic':
+        cc = Cubic(False)
+        cc.test_on_traces(traces, [os.path.join(save_dir, cc.cc_name)
+                                   for save_dir in save_dirs], True, args.nproc)
+    elif args.cc == 'real':
+        model_path = args.models_path
+        real_save_dirs = [os.path.join(
+            save_dir, args.cc, str(args.seed)) for save_dir in save_dirs]
+        test_on_traces(model_path, traces, real_save_dirs,
+                       args.nproc, 42, False, plot_flag=True)
+    elif args.cc == 'udr1' or args.cc == 'udr2' or args.cc == 'udr3':
+        # TODO: bug here when there is no validation log
+        # original implementation
+        # val_log = pd.read_csv(os.path.join(
+        #     args.models_path, 'validation_log.csv'), sep='\t')
+        # for idx in np.linspace(0, len(val_log['num_timesteps']) / 2 - 1, 10):
+        #     step = int(val_log['num_timesteps'].iloc[int(idx)])
+        #     udr_seed = ''
+        #     for s in args.models_path.split('/'):
+        #         if 'seed' in s:
+        #             udr_seed = s
+        #     udr_save_dirs = [os.path.join(
+        #         save_dir, args.cc, udr_seed, "step_{}".format(step)) for save_dir in save_dirs]
+        #     model_path = os.path.join(
+        #         args.models_path, 'model_step_{}.ckpt'.format(step))
+        #     test_on_traces(model_path, traces, udr_save_dirs,
+        #                    args.nproc, 42, False, False)
+        # new implementation to be fair with genet
+        udr_seed = ''
+        for s in args.models_path.split('/'):
+            if 'seed' in s:
+                udr_seed = s
+        step = 64800
+        while True:
+            if not os.path.exists(os.path.join(args.models_path, 'model_step_{}.ckpt.meta'.format(step))):
+                break
+            udr_save_dirs = [os.path.join(
+                save_dir, args.cc, udr_seed, "step_{}".format(step)) for save_dir in save_dirs]
+            model_path = os.path.join(
+                args.models_path, 'model_step_{}.ckpt'.format(step))
+            test_on_traces(model_path, traces, udr_save_dirs,
+                           args.nproc, 42, False, True)
+            step += 64800
+    elif args.cc == 'genet_bbr' or args.cc == 'genet_cubic' or 'genet_bbr_old':
+        genet_seed = ''
+        for s in args.models_path.split('/'):
+            if 'seed' in s:
+                genet_seed = s
+        for bo in range(0, 15, 3):
+            bo_dir = os.path.join(args.models_path, "bo_{}".format(bo))
             step = 64800
-            while True:
-                if not os.path.exists(os.path.join(args.models_path, 'model_step_{}.ckpt.meta'.format(step))):
-                    break
-                udr_save_dirs = [os.path.join(
-                    save_dir, args.cc, udr_seed, "step_{}".format(step)) for save_dir in save_dirs]
-                model_path = os.path.join(
-                    args.models_path, 'model_step_{}.ckpt'.format(step))
-                test_on_traces(model_path, traces, udr_save_dirs,
-                               args.nproc, 42, False, False)
-                step += 64800
-        elif args.cc == 'genet_bbr' or args.cc == 'genet_cubic' or 'genet_bbr_old':
-            genet_seed = ''
-            for s in args.models_path.split('/'):
-                if 'seed' in s:
-                    genet_seed = s
-            for bo in range(0, 30, 3):
-                bo_dir = os.path.join(args.models_path, "bo_{}".format(bo))
-                step = 64800
-                model_path = os.path.join(
-                    bo_dir, 'model_step_{}.ckpt'.format(step))
-                if not os.path.exists(model_path + '.meta'):
-                    continue
-                genet_save_dirs = [os.path.join(
-                    save_dir, args.cc, genet_seed, "bo_{}".format(bo),
-                    "step_{}".format(step)) for save_dir in save_dirs]
-                # print(genet_save_dirs)
-                test_on_traces(model_path, traces, genet_save_dirs,
-                               args.nproc, 42, False, False)
-        else:
-            raise ValueError
+            model_path = os.path.join(
+                bo_dir, 'model_step_{}.ckpt'.format(step))
+            if not os.path.exists(model_path + '.meta'):
+                continue
+            genet_save_dirs = [os.path.join(
+                save_dir, args.cc, genet_seed, "bo_{}".format(bo),
+                "step_{}".format(step)) for save_dir in save_dirs]
+            # print(genet_save_dirs)
+            test_on_traces(model_path, traces, genet_save_dirs,
+                           args.nproc, 42, False, True)
+    else:
+        raise ValueError
 
 
 if __name__ == "__main__":
